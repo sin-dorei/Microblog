69ab721c6d89d7bf7311445e3038dce24442a80f
sin-dorei <228827120@qq.com>
2020-12-16 11:46:13 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
